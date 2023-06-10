5a14b909a069ee910a63c38789b39c730b49ce31
smunoz <smunoz@flow.cl>
2023-06-10 05:47:07 -0400

    se configura login

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,39 +2,60 @@ import React, { Component } from "react";
 import usuario from "../assets/images/usuario.png";
 import { NavLink } from "react-router-dom";
 
-class Header extends Component {
+function Header() {
+    let logged = sessionStorage.getItem('logged');
 
-    render() {
-        return (
-            <header id="header">
-                <div className="center">
-                    <div id="nombreVet">
-                        <span id="brand">
-                            <strong>Beloved Pets</strong>
-                        </span>
-                    </div>
-
-                    {/* MENU */}
-                    <nav id="menu">
-                        <ul>
-                            <li>
-                                <NavLink to="/login">Iniciar sesión</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/registro">Registrarse</NavLink>
-                            </li>
-                            <div>
-                                <img src={usuario} className="userLogo" alt="Logotipo" />
-                            </div>
-                        </ul>
-
-                    </nav>
-                    {/*LIMPIAR FLOTADOS*/}
-                    <div className="clearfix"></div>
-                </div>
-            </header>
-        );
+    const logOut = () => {
+        sessionStorage.setItem('logged', false);
+        alert("Sesión cerrada con éxito")
+        window.location.href = '/';
+        logged = sessionStorage.getItem('logged');
     }
+
+    return (
+        <header id="header">
+            <div className="center">
+                <div id="nombreVet">
+                    <span id="brand">
+                        <strong>Beloved Pets</strong>
+                    </span>
+                </div>
+
+                {/* MENU */}
+                {logged == "true" ? <nav id="menuloggeado">
+                    <ul>
+                        <li>
+                            <NavLink to="/login">Bienvenido Sebastian</NavLink>
+                        </li>
+                        <li>
+                            <NavLink onClick={logOut}>Cerrar sesión</NavLink>
+                        </li>
+                        <div>
+                            <img src={usuario} className="userLogo" alt="Logotipo" />
+                        </div>
+                    </ul>
+
+                </nav> : <nav id="menu">
+                    <ul>
+                        <li>
+                            <NavLink to="/login">Iniciar sesión</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/registro">Registrarse</NavLink>
+                        </li>
+                        <div>
+                            <img src={usuario} className="userLogo" alt="Logotipo" />
+                        </div>
+                    </ul>
+
+                </nav>}
+                
+                {/*LIMPIAR FLOTADOS*/}
+                <div className="clearfix"></div>
+            </div>
+        </header>
+    );
+
 }
 
 export default Header;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,64 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import { NavLink } from "react-router-dom";
 
-class Login extends Component {
+function Login() {
+    const [users, setUsers] = useState([]);
+    const [nombre, setNombre] = useState('');
+    const [correo, setCorreo] = useState('');
+    const [pass, setPass] = useState('');
+    const [pass2, setPass2] = useState('');
 
-    render() {
-        return (
-            <div>
-                <span>Login</span>
-            </div>
-        );
+    const recibirFormulario = (e) => {
+        e.preventDefault();
+
+        let correoPrueba = "sebastian.munoz.z@gmail.com";
+        let passPrueba = "seba1234";
+        const user = {
+            correo: correo,
+            contraseña: pass
+        }
+        if (user.contraseña != passPrueba || user.correo != correoPrueba) {
+            alert("Correo o contraseña incorrectos, intenta de nuevo");
+        } else {
+            sessionStorage.setItem('logged', true);
+            alert("inicio de sesión exitoso");
+            window.location.href = '/';
+        }
     }
+
+    return (
+        <div id="formulario">
+            <div className="center">
+                <div id="form">
+                    <h1 className="subheader">Iniciar sesión</h1>
+
+                    <form className="mid-form" onSubmit={recibirFormulario}>
+                        <div className="form-group">
+                            <label htmlFor="correo">Correo electrónico (*)</label>
+                            <input type="text" name="correo" onChange={(event) => setCorreo(event.target.value)} />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="pass">Contraseña (*)</label>
+                            <input type="text" name="pass" onChange={(event) => setPass(event.target.value)} />
+                        </div>
+
+                        <h4 className="dato-obligatorio">(*) Los datos marcados son obligatorios</h4>
+
+                        <div className="clearfix"></div>
+
+                        <div>
+                            <NavLink type="" value="Cancelar" className="btn btn-cancel" to="/">Cancelar</NavLink>
+                            <input type="submit" value="Iniciar sesión" className="btn btn-success" />
+                        </div>
+
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+
 }
 
 export default Login;
